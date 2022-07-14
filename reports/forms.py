a1b216c463b5f887090108a25cc8623b063f120f
Zhanviss <assemzhar@icloud.com>
2022-07-14 19:58:26 +0600

    Adding Subjects Dependent DropDown list

--- reports/forms.py
+++ reports/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Report
-from groups.models import Group
+from groups.models import GroupLinkProfessorSubject
 from students.models import Student
 class ReportForm(forms.ModelForm):
     class Meta:
@@ -18,3 +18,13 @@ class ReportForm(forms.ModelForm):
                 pass  # invalid input from the client; ignore and fallback to empty City queryset
         elif self.instance.pk:
             self.fields['student_1to1'].queryset = self.instance.group_1to1.student_1to1_set
+        
+        self.fields['subject_1to1'].queryset = GroupLinkProfessorSubject.objects.none()
+        if 'group' in self.data:
+            try:
+                group_id = int(self.data.get('group_1to1'))
+                self.fields['subject_1to1'].queryset = GroupLinkProfessorSubject.objects.filter(group_fk = group_id)
+            except (ValueError, TypeError):
+                pass  # invalid input from the client; ignore and fallback to empty City queryset
+        elif self.instance.pk:
+            self.fields['subject_1to1'].queryset = self.instance.group_1to1.subject_1to1_set
--- reports/urls.py
+++ reports/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ReportList, ReportDetail, ReportCreate, load_students
+from .views import ReportList, ReportDetail, ReportCreate, load_students, load_subjects
 urlpatterns = [
     path('', ReportList.as_view(), name='report-list'),
     path('<int:pk>/', ReportDetail.as_view(), name='report-detail'),
     path('new/', ReportCreate.as_view(), name='report-new'),
 
     path('ajax/load-students/', load_students, name='ajax_load_students'),
+    path('ajax/load-subjects/', load_subjects, name='ajax_load_subjects'),
 ]
