eaea0a99fc05664c95b67a19a73fc2fca47bb3d9
Zhanviss <assemzhar@icloud.com>
2022-07-14 18:22:37 +0600

    Adding views and html templates

---
+++ reports/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ReportList, ReportDetail, ReportCreate
+urlpatterns = [
+    path('', ReportList.as_view(), name='report-list'),
+    path('<int:pk>/', ReportDetail.as_view(), name='report-detail'),
+    path('new/', ReportCreate.as_view(), name='report-new')
+]
--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView, DetailView
+from .models import Report
+from students.models import Student
 # Create your views here.
+#models.TextField(default=GroupLinkProfessorSubject.objects.all().filter(self.student_1to1.group_fk == GroupLinkProfessorSubject.group_fk).__str__())
+
+class ReportList(ListView):
+    model = Report
+    template_name = "report_list.html"
+
+class ReportDetail(DetailView):
+    model = Report 
+    template_name = "report_detail.html"
+
+class ReportCreate(CreateView):
+    model = Report 
+    template_name = "report_new.html"
+    fields = ('student_1to1', 'document_pdf')
